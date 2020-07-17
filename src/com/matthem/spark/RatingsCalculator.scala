707b54d8b1c6bac0868259524cba9593051622cb
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-17 00:41:56 -0400

    Add script to get friends by age

--- src/com/matthem/spark/RatingsCalculator.scala
+++ src/com/matthem/spark/RatingsCalculator.scala
@@ -1,7 +1,6 @@
 package com.matthem.spark
 
 import org.apache.spark._
-import org.apache.spark.SparkContext._
 import org.apache.log4j._
 
 object RatingsCalculator {
@@ -19,7 +18,7 @@ object RatingsCalculator {
 
     // Convert each line to a string, split it out by tabs, and extract the third field.
     // (The file format is userID, movieID, rating, timestamp)
-    val ratings = lines.map(x => x.toString().split("\t")(2))
+    val ratings = lines.map(x => x.split("\t")(2))
 
     // Count up how many times each value (rating) occurs
     val results = ratings.countByValue()
